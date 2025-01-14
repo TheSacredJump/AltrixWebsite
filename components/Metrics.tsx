113ea1d4873affea85de1d14e51ad237e2722461
greentreegroup <86478969+greentreegroup@users.noreply.github.com>
2025-01-14 01:25:21 +0100

    Update for impact tab

--- components/Metrics.tsx
+++ components/Metrics.tsx
@@ -52,7 +52,7 @@ export default function FeaturesSectionDemo() {
           className="px-8"
         >
           <h4 className="text-3xl lg:text-5xl lg:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium text-black dark:text-white">
-            The Altrix Difference:
+            Built for Enterprise
           </h4>
   
           <p className="text-sm lg:text-base max-w-2xl my-4 mx-auto text-neutral-500 text-center font-normal dark:text-neutral-300">
