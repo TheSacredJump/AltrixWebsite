f8861168f54203d7447bb795257a73e838bc027e
greentreegroup <86478969+greentreegroup@users.noreply.github.com>
2025-01-21 03:53:14 +0100

    fixed spelling

--- components/TrustedBy.tsx
+++ components/TrustedBy.tsx
@@ -19,7 +19,7 @@ const MarqueeTrustedBy = () => {
     <section className="py-20 overflow-hidden dark:bg-black" id="about">
       <div  className="container mx-auto px-4">
         <h2 className="text-3xl md:text-4xl font-bold text-center mb-20">
-          Co-Developed with Leading <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#6677db] to-violet-500"> RNs, CNOs, CNIOs, & Engineers</span>
+          Co-Developed with Leading <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#6677db] to-violet-500"> RNs, CNOs, & Engineers</span>
         </h2>
         <div className="relative">
           <div className="flex animate-marquee">
