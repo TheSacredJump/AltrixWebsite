81156e66dd975ec0324b41b496bf7cdbc25dea44
greentreegroup <86478969+greentreegroup@users.noreply.github.com>
2025-01-21 03:37:14 +0100

    turned off dark mode

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -78,29 +78,18 @@ const Navbar = () => {
             </Link>
             <div className="hidden md:flex md:space-x-8 ml-10">
               <NavLink href="/#features">Platform</NavLink>
-              <NavLink href="/#nursebenefits">Impact</NavLink>
               <NavLink href="/#about">About</NavLink>
-              <NavLink href="/whitepaper">Research</NavLink>
+              <NavLink href="/#enterprise">Enterprise</NavLink>
+              <NavLink href="https://independent.academia.edu/HectorBenitezVentura">Research</NavLink>
             </div>
           </div>
           <div className="hidden md:flex items-center space-x-4">
             <Link href="/waitlist" className={`inline-flex items-center px-4 py-2 border border-[#6677db] text-sm font-medium rounded-md text-[#6677db] ${isOnDarkSection ? 'bg-black' : 'bg-white dark:bg-neutral-950'} hover:bg-[#6677db] dark:hover:bg-[#6677db] hover:text-white hover:scale-105 transition duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500`}>
             Supercharge Your Nurses
             </Link>
-            <button
-              onClick={toggleDarkMode}
-              className="p-2 rounded-full text-gray-600 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none"
-            >
-              {isDarkMode ? <Sun className="h-5 w-5" /> : <Moon className="h-5 w-5" />}
-            </button>
+            
           </div>
           <div className="md:hidden flex items-center space-x-4">
-            <button
-              onClick={toggleDarkMode}
-              className="p-2 rounded-full text-gray-600 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none"
-            >
-              {isDarkMode ? <Sun className="h-5 w-5" /> : <Moon className="h-5 w-5" />}
-            </button>
             <button
               onClick={toggleMenu}
               className="inline-flex items-center justify-center p-2 rounded-md text-[#6677db] focus:outline-none"
@@ -115,9 +104,9 @@ const Navbar = () => {
         <div className="md:hidden shadow-md">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 bg-white dark:bg-neutral-950">
             <MobileNavLink href="/#features">Platform</MobileNavLink>
-            <MobileNavLink href="/#nursebenefits">Impact</MobileNavLink>
-            <MobileNavLink href="/#about">About</MobileNavLink>
-            <MobileNavLink href="/whitepaper">Research</MobileNavLink>
+            <MobileNavLink href="/#about">Impact</MobileNavLink>
+            <MobileNavLink href="/#enterprise">About</MobileNavLink>
+            <MobileNavLink href="https://independent.academia.edu/HectorBenitezVentura">Research</MobileNavLink>
             <Link href="/waitlist" className="block w-full text-center px-3 py-2 rounded-md text-base font-medium text-white bg-[#6677db] hover:bg-violet-700">
               Supercharge Your Nurses
             </Link>
